3d2138e7355daad175973bc4e24e076f5ce19ce9
Vadim <lyzhinv@gmail.com>
2022-11-15 22:43:31 +0300

    Add Exercise 64

---
+++ Ex_64/Ex_64.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"N = {n} -> {GetNumbers(n, 1)}");
+
+string GetNumbers(int start, int end)
+{
+    if (start == end) return start.ToString();
+    return start + ", " + GetNumbers(start - 1, end);
+}
